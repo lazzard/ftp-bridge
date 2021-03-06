5fd4053250341dd8141d54eeb0d0116825aefc58
AmraniCh <elamrani.sv.laza@gmail.com>
2021-03-06 17:09:43 +0100

    FtpBridgeInterface removed

--- src/FtpBridge.php
+++ src/FtpBridge.php
@@ -1,4 +1,5 @@
-<?php
+<?
+
 /**
  * This file is part of the Lazzard/ftp-bridge package.
  *
@@ -17,13 +18,10 @@ use Lazzard\FtpBridge\Stream\CommandStream;
 use Lazzard\FtpBridge\Stream\DataStream;
 
 /**
- * FtpBridge class holds the necessary methods to start sending commands and
- * receiving FTP responses with some already implemented methods.
- *
  * @since  1.0
  * @author El Amrani Chakir <elamrani.sv.laza@gmail.com>
  */
-class FtpBridge implements FtpBridgeInterface
+class FtpBridge
 {
     /**
      * Transfers modes.
@@ -52,7 +50,11 @@ class FtpBridge implements FtpBridgeInterface
     }
 
     /**
-     * @inheritDoc
+     * Sends a command to the server thought the control channel.
+     *
+     * @param string $command
+     *
+     * @return bool
      */
     public function send($command)
     {
@@ -60,15 +62,19 @@ class FtpBridge implements FtpBridgeInterface
     }
 
     /**
-     * @inheritDoc
+     * Receives and gets the response from the command stream.
+     *
+     * @return FtpResponse
      */
-    public function receive($dump = false)
+    public function receive()
     {
-        return new FtpResponse($this->commandStream->receive($dump));
+        return new FtpResponse($this->commandStream->receive());
     }
 
     /**
-     * @inheritDoc
+     * Receives and reads the data from the data stream.
+     *
+     * @return string
      */
     public function receiveData()
     {
@@ -76,7 +82,15 @@ class FtpBridge implements FtpBridgeInterface
     }
 
     /**
-     * {@inheritDoc}
+     * Opens a command stream connection.
+     *
+     * @param string $host     The remote host name or the IP address.
+     * @param int    $port     [optional] The remote server port to connect to, if omitted the port 21 will be used.
+     * @param int    $timeout  [optional] Specifies the connection timeout of all FTP transfer operations, default sets
+     *                         to 90.
+     * @param        $blocking $blocking [optional] The transfer mode, the blocking mode is the default.
+     *
+     * @return bool Returns true on success, false on failure and an E_WARNING error raised.
      */
     public function connect($host, $port = 21, $timeout = 90, $blocking = true)
     {
@@ -85,56 +99,54 @@ class FtpBridge implements FtpBridgeInterface
     }
 
     /**
-     * {@inheritDoc}
+     * Logs into the FTP server.
+     *
+     * Note: this method must be called after a successful connection.
+     *
+     * @param string $username
+     * @param string $password
+     *
+     * @return bool Returns true on success, false on failure and an E_WARNING error raised.
      */
     public function login($username, $password)
     {
         $this->send(sprintf("USER %s", $username));
         $response = $this->receive();
 
-        /**
-         * 230 : User logged in, proceed.
-         *
-         */
         if (in_array($response->getCode(), array(230))) {
             return true;
         }
 
-        /**
-         * 331 : User name okay, need password.
-         */
         if (in_array($response->getCode(), array(331))) {
-            //$this->receive();
             $this->send(sprintf('PASS %s', $password));
-
-            $response = $this->receive();
-
-            // TODO 202 code
-            /**
-             * 230 : User logged in, proceed.
-             * 202 : Already logged with USER
-             */
-            if (in_array($response->getCode(), array(202, 230))) {
+            if (in_array($this->receive()->getCode(), array(202, 230))) { // TODO 202 code
                 return true;
             }
-
             return !trigger_error(sprintf("PASS command failed : %s", $response->getMessage()), E_USER_WARNING);
         }
-
         return !trigger_error(sprintf("USER command failed : %s", $response->getMessage()), E_USER_WARNING);
     }
 
     /**
-     * {@inheritDoc}
+     * Opens the data connection.
+     *
+     * @param bool $passive [optional] Specifies weather to use a passive or active data connection.
+     *
+     * @return bool Returns true on success, false on failure and an E_WARNING error raised.
      */
-    public function openDataConnection($passive = true)
+    public function openDataConnection($passive = false)
     {
         $this->dataStream = new DataStream($this->logger, $this->commandStream, $passive);
         return $this->dataStream->open();
     }
 
     /**
-     * @inheritDoc
+     * Sets the transfer type for the next transfer operation.
+     *
+     * @param string $type The transfer type can be either {@link FtpBridge::BINARY} or {@link FtpBridge::ASCII} or
+     *                     {@link FtpBridge::EBCDIC}.
+     *
+     * @return void
      */
     public function setTransferType($type)
     {
