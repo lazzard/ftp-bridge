fe0e7ccf9bedffea2879fe2dc0c717916e6736ba
AmraniCh <elamrani.sv.laza@gmail.com>
2021-08-08 11:58:20 +0100

    ExceptionTestCase Added

---
+++ tests/Exception/ExceptionTestCase.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Lazzard\FtpBridge\Tests\Exception;
+
+use PHPUnit\Framework\TestCase;
+use Lazzard\FtpBridge\Exception\FtpBridgeException;
+
+class ExceptionTestCase extends TestCase
+{
+    /** @var string */
+    protected $exceptionClass;
+
+    public function testThrow()
+    {
+        $this->expectException($this->exceptionClass);
+        $this->expectExceptionMessage("some exception message.");
+
+        throw new $this->exceptionClass("some exception message.");
+    }
+
+    public function testExceptionExtendsFtpBridgeException()
+    {
+        $this->assertTrue(new $this->exceptionClass("some exception message.") instanceof FtpBridgeException);
+    }
+}
