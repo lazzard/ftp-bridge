00483e864dfa01f1308f97cd26fd11ace22fc187
AmraniCh <elamrani.sv.laza@gmail.com>
2021-08-11 04:58:29 +0100

    ResponseParserTest updated

--- tests/Response/ResponseParserTest.php
+++ tests/Response/ResponseParserTest.php
@@ -1,128 +1,144 @@
 <?php
 
-namespace Response;
+namespace Lazzard\FtpBridge\Tests\Response;
 
+use Lazzard\FtpBridge\FtpBridge;
 use Lazzard\FtpBridge\Response\ResponseParser;
 use PHPUnit\Framework\TestCase;
 use ReflectionClass;
 
 class ResponseParserTest extends TestCase
 {
-    public function testConstructor()
+    public function testToArrayWithMultilineReply()
     {
-        $parser = $this->getMockBuilder(ResponseParser::class)
-            ->disableOriginalConstructor()
-            ->getMock();
+        $crlf = FtpBridge::CRLF;
 
-        $this->assertInstanceOf(ResponseParser::class, $parser);
+        $reply = "214-The following commands are recognized (* =>'s unimplemented):{$crlf}" .
+            "CWD     XCWD    CDUP    XCUP    SMNT*   QUIT    PORT    PASV{$crlf}" .
+            "EPRT    EPSV    ALLO*   RNFR    RNTO    DELE    MDTM    RMD{$crlf}" .
+            "XRMD    MKD     XMKD    PWD     XPWD    SIZE    SYST    HELP{$crlf}" .
+            "NOOP    FEAT    OPTS    HOST    CLNT    AUTH    CCC*    CONF*{$crlf}" .
+            "ENC*    MIC*    PBSZ    PROT    TYPE    STRU    MODE    RETR{$crlf}" .
+            "STOR    STOU    APPE    REST    ABOR    USER    PASS    ACCT*{$crlf}" .
+            "REIN*   LIST    NLST    STAT    SITE    MLSD    MLST{$crlf}" .
+            "214 Direct comments to root@localhost{$crlf}";
+
+        $response = (new ResponseParser($reply))->toArray();
+
+        $this->assertSame([
+            'code'      => 214,
+            'message'   => 'The following commands are recognized (* =>\'s unimplemented):',
+            'multiline' => true,
+        ], $response);
     }
 
-    public function testParseWithMultilineResponse()
+    public function testToArrayWithOneLineReply()
     {
-        // testing with response is multiline
-        $rawResponse = "214-The following SITE commands are recognized
- ALIAS
- CHMOD
- IDLE
- UTIME
-214 Pure-FTPd - http://pureftpd.org/
-";
+        $reply = "227 Entering Passive Mode (109,106,246,248,140,108).";
 
-        $parser   = new ResponseParser($rawResponse);
-        $response = $parser->toArray();
+        $response = (new ResponseParser($reply))->toArray();
 
-        $this->assertSame(214, $response['code']);
-        $this->assertSame("The following SITE commands are recognized", $response['message']);
-        $this->assertTrue($response['multiline']);
+        $this->assertSame([
+            'code'      => 227,
+            'message'   => 'Entering Passive Mode (109,106,246,248,140,108).',
+            'multiline' => false,
+        ], $response);
     }
 
-    public function testParseWithOneLineResponse()
+    public function testParseCodeReturnsIntegerWithAValidReply()
     {
-        $rawResponse = "200 Zzz..";
-
-        $parser   = new ResponseParser($rawResponse);
-        $response = $parser->toArray();
-
-        $this->assertSame(200, $response['code']);
-        $this->assertSame(" Zzz..", $response['message']);
-        $this->assertFalse($response['multiline']);
-    }
-
-    public function testParseCodeWithRegularResponse()
-    {
-        $rawResponse = "200 Zzz..";
+        $reply = "227 Entering Passive Mode (109,106,246,248,140,108).";
 
         $method = self::getMethod('parseCode');
-        $parser = new ResponseParser($rawResponse);
+        $parser = new ResponseParser($reply);
 
-        $this->assertSame(200, $method->invoke($parser));
+        $this->assertSame(227, $method->invoke($parser));
     }
 
-    public function testParseCodeWithNoCodeInResponse()
+    public function testParseCodeReturnsFalseWithAnInvalidReply()
     {
-        $rawResponse = "foo";
+        $reply = "invalid ftp reply";
 
         $method = self::getMethod('parseCode');
-        $parser = new ResponseParser($rawResponse);
+        $parser = new ResponseParser($reply);
 
         $this->assertFalse($method->invoke($parser));
     }
 
-    public function testIsMultilineWithMultilineResponse()
+    public function testParseMessageReturnsStringWithOneLineReply()
     {
-        $rawResponse = "214-The following SITE commands are recognized
- ALIAS
- CHMOD
- IDLE
- UTIME
-214 Pure-FTPd - http://pureftpd.org/";
+        $reply = "227 Entering Passive Mode (109,106,246,248,140,108).";
+
+        $method = self::getMethod('parseMessage');
+        $parser = new ResponseParser($reply);
+
+        $this->assertSame("Entering Passive Mode (109,106,246,248,140,108).", $method->invoke($parser));
+    }
+
+    public function testParseMessageReturnsStingWithMultilineReply()
+    {
+        $crlf = FtpBridge::CRLF;
+
+        $reply = "214-The following commands are recognized (* =>'s unimplemented):{$crlf}" .
+            "CWD     XCWD    CDUP    XCUP    SMNT*   QUIT    PORT    PASV{$crlf}" .
+            "EPRT    EPSV    ALLO*   RNFR    RNTO    DELE    MDTM    RMD{$crlf}" .
+            "XRMD    MKD     XMKD    PWD     XPWD    SIZE    SYST    HELP{$crlf}" .
+            "NOOP    FEAT    OPTS    HOST    CLNT    AUTH    CCC*    CONF*{$crlf}" .
+            "ENC*    MIC*    PBSZ    PROT    TYPE    STRU    MODE    RETR{$crlf}" .
+            "STOR    STOU    APPE    REST    ABOR    USER    PASS    ACCT*{$crlf}" .
+            "REIN*   LIST    NLST    STAT    SITE    MLSD    MLST{$crlf}" .
+            "214 Direct comments to root@localhost{$crlf}";
+
+        $method = self::getMethod('parseMessage');
+        $parser = new ResponseParser($reply);
+
+        $this->assertSame("The following commands are recognized (* =>'s unimplemented):", $method->invoke($parser));
+    }
+
+    public function testParseMessageReturnsFalseWithAnInvalidReply()
+    {
+        $reply = "invalid ftp reply";
+
+        $method = self::getMethod('parseMessage');
+        $parser = new ResponseParser($reply);
+
+        $this->assertFalse($method->invoke($parser));
+    }
+
+    public function testIsMultilineReturnsFalseWithOneLineReply()
+    {
+        $method = self::getMethod('isMultiline');
+
+        $parser = new ResponseParser("227 Entering Passive Mode (109,106,246,248,140,108).");
+
+        $this->assertFalse($method->invoke($parser));
+    }
+
+    public function testIsMultilineReturnsTrueWithMultilineReply()
+    {
+        $crlf = FtpBridge::CRLF;
+
+        $reply = "214-The following commands are recognized (* =>'s unimplemented):{$crlf}" .
+            "CWD     XCWD    CDUP    XCUP    SMNT*   QUIT    PORT    PASV{$crlf}" .
+            "EPRT    EPSV    ALLO*   RNFR    RNTO    DELE    MDTM    RMD{$crlf}" .
+            "XRMD    MKD     XMKD    PWD     XPWD    SIZE    SYST    HELP{$crlf}" .
+            "NOOP    FEAT    OPTS    HOST    CLNT    AUTH    CCC*    CONF*{$crlf}" .
+            "ENC*    MIC*    PBSZ    PROT    TYPE    STRU    MODE    RETR{$crlf}" .
+            "STOR    STOU    APPE    REST    ABOR    USER    PASS    ACCT*{$crlf}" .
+            "REIN*   LIST    NLST    STAT    SITE    MLSD    MLST{$crlf}" .
+            "214 Direct comments to root@localhost{$crlf}";
 
         $method = self::getMethod('isMultiline');
-        $parser = new ResponseParser($rawResponse);
+        $parser = new ResponseParser($reply);
 
         $this->assertTrue($method->invoke($parser));
-    }
-
-    public function testIsMultilineWithOneLineResponse()
-    {
-        $rawResponse = "200 Zzz..";
-
-        $method = self::getMethod('isMultiline');
-        $parser = new ResponseParser($rawResponse);
-
-        $this->assertFalse($method->invoke($parser));
-    }
-
-    public function testParseMessageWithOneLineResponse()
-    {
-        $rawResponse = "200 Zzz..";
-
-        $method = self::getMethod('parseMessage');
-        $parser = new ResponseParser($rawResponse);
-
-        $this->assertSame(" Zzz..", $method->invoke($parser));
-    }
-
-    public function testParseMessageWithMultilineResponse()
-    {
-        $rawResponse = "214-The following SITE commands are recognized
- ALIAS
- CHMOD
- IDLE
- UTIME
-214 Pure-FTPd - http://pureftpd.org/";
-
-        $method = self::getMethod('parseMessage');
-        $parser = new ResponseParser($rawResponse);
-
-        $this->assertSame("The following SITE commands are recognized", $method->invoke($parser));
     }
 
     protected static function getMethod($name)
     {
         $class  = new ReflectionClass(ResponseParser::class);
         $method = $class->getMethod($name);
-        
+
         $method->setAccessible(true);
         return $method;
     }
